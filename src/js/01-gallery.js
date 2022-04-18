d07a8d703a1aa4c9f91ecef1d9164bf91cdf0ea8
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2022-04-18 17:10:29 +0300

    01-hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imageGalleryList = document.querySelector('.gallery');
+const galleryMarkup = createsImageGalleryMarkup(galleryItems);
+
+imageGalleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createsImageGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+      </a>
+  `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
